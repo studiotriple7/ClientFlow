4958dcb210827f50e0b83ada5e6028f59264a940
studiotriple7 <anthony@studiotriple7.com>
2025-11-01 22:42:17 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDFCaraph7uBPnuAoPy7ujBfNaLP",
-  authDomain: "clientflow-2ec7f.firebaseapp.com",
-  projectId: "clientflow-2ec7f",
-  storageBucket: "clientflow-2ec7f.firebasestorage.app",
-  messagingSenderId: "1897293119",
-  appId: "1:1897293119:web:54a6e4fdf0afaf2e423a"
+  apiKey: "AIzaSyBUb-be4Wu4BwCTGOGhN0kc40Cnl3G7Ez4",
+  authDomain: "clientflow-7acff.firebaseapp.com",
+  projectId: "clientflow-7acff",
+  storageBucket: "clientflow-7acff.firebasestorage.app",
+  messagingSenderId: "16972939119",
+  appId: "1:16972939119:web:e4ea8cff095afa4f24329a"
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
