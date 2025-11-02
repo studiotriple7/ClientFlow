5ef4b27e4cfdb9bf0a9300196ee3646b03dcce2f
studiotriple7 <anthony@studiotriple7.com>
2025-11-01 22:35:17 -0400

    Update firebase.js
    New Code

--- src/firebase.js
+++ src/firebase.js
@@ -3,29 +3,17 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-// YOUR FIREBASE CONFIG - PASTE YOUR CONFIG HERE
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBUb-be4Wu4BwCTGOGhN0kc40Cnl3G7Ez4",
-  authDomain: "clientflow-7acff.firebaseapp.com",
-  projectId: "clientflow-7acff",
-  storageBucket: "clientflow-7acff.firebasestorage.app",
-  messagingSenderId: "16972939119",
-  appId: "1:16972939119:web:e4ea8cff095afa4f24329a"
+  apiKey: "AIzaSyDFCaraph7uBPnuAoPy7ujBfNaLP",
+  authDomain: "clientflow-2ec7f.firebaseapp.com",
+  projectId: "clientflow-2ec7f",
+  storageBucket: "clientflow-2ec7f.firebasestorage.app",
+  messagingSenderId: "1897293119",
+  appId: "1:1897293119:web:54a6e4fdf0afaf2e423a"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
