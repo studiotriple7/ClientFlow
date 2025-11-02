2111a71296bcf7816978387673f741f8e8569ad2
studiotriple7 <anthony@studiotriple7.com>
2025-11-01 22:23:16 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,23 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // YOUR FIREBASE CONFIG - PASTE YOUR CONFIG HERE
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBUb-be4Wu4BwCTGOGhN0kc40Cnl3G7Ez4",
+  authDomain: "clientflow-7acff.firebaseapp.com",
+  projectId: "clientflow-7acff",
+  storageBucket: "clientflow-7acff.firebasestorage.app",
+  messagingSenderId: "16972939119",
+  appId: "1:16972939119:web:e4ea8cff095afa4f24329a"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
